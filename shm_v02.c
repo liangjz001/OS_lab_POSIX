a9e9a53b08df2616cce98f043769a3795ac87dd9
梁建中 <liangjianzhong@liangjianzhongdeMacBook-Pro.local>
2018-01-22 16:22:37 +0100

    posix

--- shm_v02.c
+++ shm_v02.c
@@ -6,6 +6,12 @@
 #include <pthread.h>
 
 #define FINAL_VALUE 20
+#define N_WRITER 1
+#define N_READER 1
+
+
+void *writring(void *arg);
+void *reading(void *arg)
 
 main(int argc, char * argv[]) {
   int        fd;
@@ -40,53 +46,56 @@ main(int argc, char * argv[]) {
   close(fd);
 
   *addr = 0;
+  
+  pthread_t tid_W[N_WRITER];
+  pthread_t tid_R[N_READER];
 
-void writring(){
-    int i;
-    while(1){
-	if (i == FINAL_VALUE){
 
+  int j = 0;
+  for (j=0; j<N_WRITER; j++){
+    pthread_create(&tid_W[j], NULL, writring, NULL);
+    pthread_join(&tid_W[j], NULL);
+  }
+  for (j=0; j<N_READER; j++){
+    pthread_create(&tid_R[j], NULL, reading, NULL);
+    pthread_join(&tid_R[j], NULL);
+  }
+
+}
+
+
+
+void *writring(void *arg){
+  int i =0;
+  while(1){
+    if (i == FINAL_VALUE){
+      printf("the writring thread is exiting\n", );
+      pthread_exit();
+    }
+    if (*addr == 0) {
+       printf("%d ->\n", i);
+       *addr = i;
+       i++;
+     }
+     sleep(random()%5);
+   }
+ }
+
+
+
+
+void *reading(void *arg){
+  while(1){
+    if (*addr == FINAL_VALUE) {
+      printf("the reading thread is exiting\n", );
+      pthread_exit();        
+    }
+    if(*addr != 0) {
+      printf("%d <-\n", *addr);
+      *addr = 0;
+  }
+ }
+ sleep(random()%5);
 }
 		
 	
-    for (i=0; i<FINAL_VALUE+1;){
-       if (*addr == 0) {
-	 printf("%d ->\n", i);	 
-	 // put value i at address addr
-         *addr = i;
-	 i++;
-	 sleep( random ( ) % 5 ) ;
-       }
-
-}
-
-
-  if (fork()) {
-    int i;
-     for (i=0; i<FINAL_VALUE+1;)
-       if (*addr == 0) {
-	 printf("%d ->\n", i);
-	 
-	 // put value i at address addr
-         *addr = i;
-
-	 i++;
-       } else
-	 // call scheduler
-         sched_yield();
-  } else
-    for (;;)
-      if (*addr != 0) {
-	 printf("%d <-\n", *addr);
-	 if (*addr == FINAL_VALUE) {
-	   exit(0);
-	 }
-         // put value 0 at address addr
-	 *addr = 0;
-
-      }else {
-	// call scheduler
-	sched_yield();
-
-      }
-}
