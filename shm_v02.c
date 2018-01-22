14cf24ecc0be16b38cf437f54c704a197ad18dda
梁建中 <liangjianzhong@liangjianzhongdeMacBook-Pro.local>
2018-01-22 16:40:29 +0100

    globe variable

--- shm_v02.c
+++ shm_v02.c
@@ -12,40 +12,10 @@
 
 void *writring(void *arg);
 void *reading(void *arg);
+unsigned globle_count;
+
 
 main(int argc, char * argv[]) {
-  int        fd;
-  unsigned  *addr;
-  //char* name =;
-
-  /* Create a new memory object */
-  fd = shm_open("/mymem1", O_RDWR | O_CREAT, 0777);
-  if (fd == -1) {
-    perror("Open failed");
-    exit(0);
-  }
-
-  /* Set the memory object's size */
-  if (ftruncate(fd, sizeof(*addr)) == -1) {
-    
-    perror("ftruncate error");
-    exit(0);
-  }
-  //printf("sizeof(*addr) =%d\n",sizeof(*addr));sizeof(*addr) =4
-
-  /* Map the memory object */
-  addr = (unsigned *)(mmap(0, sizeof(*addr), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
-  if (addr == MAP_FAILED) {
-     perror("mmap failed");
-     exit(0);
-  }
-
-  printf("Map addr is %6.6X\n", addr);
-
-  // Remove the link to the memory
-  close(fd);
-
-  *addr = 0;
 
   pthread_t tid_W[N_WRITER];
   pthread_t tid_R[N_READER];
@@ -69,12 +39,12 @@ void *writring(void *arg){
   int i =0;
   while(1){
     if (i == FINAL_VALUE){
-      printf("the writring thread is exiting\n", );
+      printf("the writring thread is exiting\n" );
       pthread_exit();
     }
-    if (*addr == 0) {
+    if (globle_count == 0) {
        printf("%d ->\n", i);
-       *addr = i;
+       globle_count = i;
        i++;
      }
      sleep(random()%5);
@@ -86,13 +56,13 @@ void *writring(void *arg){
 
 void *reading(void *arg){
   while(1){
-    if (*addr == FINAL_VALUE) {
-      printf("the reading thread is exiting\n", );
+    if (globle_count == FINAL_VALUE) {
+      printf("the reading thread is exiting\n" );
       pthread_exit();        
     }
-    if(*addr != 0) {
-      printf("%d <-\n", *addr);
-      *addr = 0;
+    if(globle_count != 0) {
+      printf("%d <-\n", globle_count);
+      globle_count = 0;
   }
  }
  sleep(random()%5);
